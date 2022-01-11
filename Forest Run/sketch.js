a287b760dcfba0a1b2dea8c8fda5881b479d9e04
bindu-shree <58688775+bindu-shree@users.noreply.github.com>
2022-01-11 20:29:42 +0530

    Update sketch.js

--- Forest Run/sketch.js
+++ Forest Run/sketch.js
@@ -4,12 +4,15 @@ var  gameState;
 var form, player, game;
 var steve,steveImage,steveImg;
 var bg,index;
+var enemy,enemyImage,enemyGroup;
+var score=0;
 //BP
 function preload() {
   backgroundImage = loadImage("assets/bg1.jpg");
   steveImg=loadImage("assets/m2.png");
   steveImage=loadAnimation("assets/m1.png","assets/m2.png","assets/m3.png","assets/m4.png");
   steveImage.frameDelay=20; 
+  enemyImage=loadImage("assets/enemy.png");
 }
 
 
@@ -18,7 +21,7 @@ function setup(){
   bg=createSprite(width/2,height/2,width,height);
   bg.addImage(backgroundImage);
   bg.scale=2;
- 
+ enemyGroup = new Group ();
   game = new Game();
  
   game.start();
@@ -30,9 +33,11 @@ steve.addAnimation("running",steveImage);
 function draw(){
   background(0);
 
+
 camera .position.x=steve.position.x+400;
 if(gameState==="play"){
 game.play();
+enemies();
 }
   
   if(keyDown("w")){
@@ -43,5 +48,30 @@ game.play();
     steve.position.y+=1;
     steve.changeAnimation("running",steveImage);
   }
+  
   drawSprites();
+textSize(35);
+fill ("white");
+text ("Score: "+score,width-200,100);
+}
+
+function enemies() {
+  //write code here to spawn the clouds
+  if (frameCount % 600 === 0) {
+   enemy = createSprite(random(width-300,width-100),height-200,40,10);
+    
+    enemy.addImage(enemyImage);
+    enemy.scale = 0.5;
+    enemy.velocityX = -3;
+    
+     //assign lifetime to the variable
+    enemy.lifetime = 100;
+    
+    //adjust the depth
+  
+    
+    //add each cloud to the group
+    enemyGroup.add(enemy);
+  }
+  
 }
